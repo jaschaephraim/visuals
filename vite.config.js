6d0de8ded4e3ec9ed771abe14d93442f506ddb4b
Jascha Ephraim <jaschaephraim@gmail.com>
2023-12-28 13:34:56 -0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import glsl from 'vite-plugin-glsl';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig((config) => {
   const baseConfig = {
-    plugins: [glsl(), tsconfigPaths()],
+    plugins: [glsl(), tsconfigPaths(), dts({ rollupTypes: true })],
   };
 
   if (config.command !== 'build') {
@@ -19,6 +20,7 @@ export default defineConfig((config) => {
         name: 'visuals',
         entry: './src/main.ts',
         formats: ['es'],
+        fileName: 'index',
       },
       rollupOptions: {
         external: ['gpu-io'],
